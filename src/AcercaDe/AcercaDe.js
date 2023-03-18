290d1a894c5a69f582194bf1405417cd9f158777
AlejaFlorez <alejaflorez.edu@gmail.com>
2023-03-17 20:42:41 -0500

    Se crea index

---
+++ src/AcercaDe/AcercaDe.js
@@ -0,0 +1,19 @@
+export function AcercaDe(){
+    return(
+        <>
+            <section>
+                <div class="container my-5">
+                    <div class="row justify-content-center ">
+                        <div class="col-12 col-md-4"> 
+                            <img src="https://firebasestorage.googleapis.com/v0/b/storeskilimanjaro0.appspot.com/o/pnglicor.png?alt=media&token=23484b1b-b490-48a9-8821-c5532e7abee2" alt="foto" class="img-fluid"></img>
+                        </div>
+                        <div class="col-12 col-md-4 text-center align-self-center"> 
+                            <h2>Stores Kilimanjaro</h2>
+                            <p>Licores Importados</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
---
+++ src/Servicios/Servicios.js
@@ -0,0 +1,23 @@
+import './Servicios.css'
+export function Servicios(){
+    return(
+        <>
+            <section class="container-fluid my-5">
+                <div class="row text-center justify-content-around">
+                    <div class="col-12 col-md-3 fondo">
+                        <h2>Servicios 1</h2>
+                        <i class="bi bi-credit-card-2-front fs-1"></i>
+                    </div>
+                    <div class="col-12 col-md-3 fondo">
+                        <h2>Servicios 2</h2>
+                        <i class="bi bi-credit-card-2-front fs-1"></i>
+                    </div>
+                    <div class="col-12 col-md-3 fondo">
+                        <h2>Servicios 3</h2>
+                        <i class="bi bi-credit-card-2-front fs-1"></i>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
